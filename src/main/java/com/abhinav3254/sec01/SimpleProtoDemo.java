c649c97e4df7942a85cf6e3ade654da60618c214
abhinav3254 <abhinav3254@gmail.com>
2025-02-26 00:13:45 +0530

    using person class from proto file

--- src/main/java/com/abhinav3254/sec01/SimpleProtoDemo.java
+++ src/main/java/com/abhinav3254/sec01/SimpleProtoDemo.java
@@ -1,5 +1,6 @@
 package com.abhinav3254.sec01;
 
+import com.abhinav3254.models.PersonOuterClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,5 +8,17 @@ public class SimpleProtoDemo {
 
     // creating logger instance
     private static final Logger log = LoggerFactory.getLogger(SimpleProtoDemo.class);
+
+    public static void main(String[] args) {
+
+        // we can't create object of this class because it's constructor is private...
+        var person =  PersonOuterClass.Person.newBuilder()
+                .setName("Abhinav")
+                .setAge(25)
+                .build();
+
+        log.info("{}",person);
+
+    }
 
 }
