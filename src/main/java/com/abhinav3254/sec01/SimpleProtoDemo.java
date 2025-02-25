9d76823b95c6316717517a0edf6554ffed334231
abhinav3254 <abhinav3254@gmail.com>
2025-02-26 00:09:36 +0530

    separating files into sections

---
+++ src/main/java/com/abhinav3254/sec01/SimpleProtoDemo.java
@@ -0,0 +1,11 @@
+package com.abhinav3254.sec01;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleProtoDemo {
+
+    // creating logger instance
+    private static final Logger log = LoggerFactory.getLogger(SimpleProtoDemo.class);
+
+}
